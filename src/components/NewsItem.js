04d015cbef2abed31b36c533b4a4d2bfd6718da3
shoukat Malik <shaukatmalikse@gmail.com>
2023-08-07 13:08:48 -0700

    adding files

---
+++ src/components/NewsItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class NewsItem extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        This is news items
+      </div>
+    )
+  }
+}
+
+export default NewsItem
